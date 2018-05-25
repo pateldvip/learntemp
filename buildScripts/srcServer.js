c0895350526c5c08ed578afef71aae906d05b229
pateldvip <pateldvip@gmail.com>
2018-05-25 15:51:07 +0530

    linting done

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,13 +1,19 @@
 //var express=require('express');
 import express from 'express';
-//var path=require('path');
 import path from 'path';
-//var open=require('open');
 import open from 'open';
+import webpack from 'webpack';
+import config from '../webpack.config.dev'
 
 const port=3000;
 //make instance of express
 const app=express();
+const compiler=webpack(config);
+//to integrate webpack with express
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}));
 //set path(first variable) to respond to upon request
 app.get('/',function(req,res){
   res.sendFile(path.join(__dirname,'../src/index.html'));
